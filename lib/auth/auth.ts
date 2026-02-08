f1425c8502242173dbc6240152c8d0bc73d97379
syamantak <132641600+Syamantak-Git@users.noreply.github.com>
2026-02-08 12:55:41 +0530

    initialize user board during login

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -3,6 +3,7 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { initializeUserBoard } from "../init-user-board";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
@@ -11,7 +12,19 @@ export const auth = betterAuth({
   database: mongodbAdapter(db, { client }),
   emailAndPassword: {
     enabled: true,
-  },
+  }, 
+  databaseHooks: {
+    user : {
+        create : {
+            after : async (user) => {
+                if(user.id) {
+                    await initializeUserBoard(user.id)
+
+                }
+            }
+        }
+    }
+  }
 });
 
 export async function getSession() {
